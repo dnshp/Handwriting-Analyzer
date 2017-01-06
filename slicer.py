c2b87025898ace7478845dc8e7a3070f23e7dee8
Dinesh Parimi <dinesh.parimi@live.com>
2017-01-05 21:51:11 -0800

    Added new writing samples, updated slicer package

--- slicer.py
+++ slicer.py
@@ -1,4 +1,6 @@
 import numpy as np
+from PIL import Image
+from analyzer import grayscale
 
 def slice(arr, slice_height, slice_width):
 	arr_height, arr_width = arr.shape
@@ -10,3 +12,11 @@ def slice(arr, slice_height, slice_width):
 			col_start += slice_width
 		row_start += slice_height
 		col_start = 0
+
+def autoslice(img, slice_height, slice_width):
+	im = Image.open(img)
+	im = grayscale(np.array(im))
+	slices = list(slice(im, slice_height, slice_width))
+	slices = [Image.fromarray(i.astype(np.uint8)) for i in slices]
+	for i in range(len(slices)):
+		slices[i].save(str(i) + ".jpg")
