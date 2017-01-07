0fb4a3eceb4a3958f4f5be0190cdcfc6a2570107
Dinesh Parimi <dinesh.parimi@live.com>
2017-01-06 23:27:52 -0800

    Minor tweaks

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 #charset = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1','2','3','4','5','6','7','8','9']
-charset = map(chr, range(65,91))
+charset = list(map(chr, range(65,91)))
 
 # "Data" is abstracted as [imgmap, character] where imgmap is a 100-length array and character is a 1-length string
 
